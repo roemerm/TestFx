dacdbe28681b140625cc9caad10a68fe627ae881
Michael Roemer <michael.roemer@syrocon.de>
2015-06-29 10:12:47 +0200

    Sourceauswahl bearbeitet

--- src/test/java/eu/unibox/handheld/testfx/CaptureScreenshot.java
+++ src/test/java/eu/unibox/handheld/testfx/CaptureScreenshot.java
@@ -1,7 +1,6 @@
 package eu.unibox.handheld.testfx;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,27 +13,18 @@ public class CaptureScreenshot extends TestWatcher {
     public void failed(Throwable t, Description description) {
         String path = "." + File.separator + "build" + File.separator + "screenshots";
         File capturePath = new File(path);
-        if (!capturePath.exists()) {
-            capturePath.mkdirs();
-        }
 
         String message = t.getMessage();
-        Pattern p = Pattern.compile("(screenshot-\\S+\\.png)");
+        Pattern p = Pattern.compile("(screenshot-[0-9-]+\\.png)");
         Matcher m = p.matcher(message);
         if (m.find()) {
             String filename = m.group(1);
-            File[] screenshoots = new File("." + File.separator).listFiles(new FilenameFilter() {
-
-                @Override
-                public boolean accept(File dir, String name) {
-                    if (name.equals(filename)) {
-                        return true;
-                    }
-                    return false;
-                }
-            });
-            File target = new File(path + File.separator + filename);
-            screenshoots[0].renameTo(target);
+            File screenshoot = new File("." + File.separator + filename);
+            if (!capturePath.exists()) {
+                capturePath.mkdirs();
+            }
+            File target = new File(capturePath, filename);
+            screenshoot.renameTo(target);
         }
     }
 }
